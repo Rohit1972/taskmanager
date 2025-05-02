3fcc0f0e828ed2a7395bed79d7b55db6d0eaf48c
Rohit1972 <rohitdogra1972@gmail.com>
2025-05-03 03:22:49 +0530

    update

--- server.js
+++ server.js
@@ -7,25 +7,24 @@ import taskRouter from "./routes/taskRoute.js";
 
 
 
+
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-connect();
-
 const app = express();
 const port = process.env.PORT || 4000
 
-
-app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 
 //DB Connect
