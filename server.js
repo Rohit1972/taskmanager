860ebbf57737cd12b732a5cb32cca669ce30b24e
Rohit1972 <rohitdogra1972@gmail.com>
2025-05-03 03:17:27 +0530

    update

--- server.js
+++ server.js
@@ -6,9 +6,21 @@ import userRouter from "./routes/userRoute.js";
 import taskRouter from "./routes/taskRoute.js";
 
 
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+connect();
+
 const app = express();
 const port = process.env.PORT || 4000
 
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 //Middleware
 app.use(cors());
